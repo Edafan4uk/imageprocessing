fd5c8ddb847ed4701488e45a45cc51375e90be7e
Eduard Zakharin <edikzaxarin@gmail.com>
2019-12-07 23:38:04 +0200

    firstandonly

---
+++ ImageProc/ImageProc/Program.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Numerics;
+
+namespace ImageProc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            using (var image = (Bitmap)Image.FromFile(args[0]))
+            {
+                EncoderParameters encoderParameters;
+
+                #region BmpRle
+                var bmpRlePath = "bmpRle.bmp";
+
+                encoderParameters = new EncoderParameters
+                {
+                    Param = new[]
+                    {
+                        new EncoderParameter(Encoder.Compression, (long)EncoderValue.CompressionRle)
+                    }
+                };
+
+                var (compRle, encodeT, decodeT, readT, writeT) = GetCompressedWithStats(image,
+                    ImageFormat.Bmp,
+                    encoderParameters,
+                    bmpRlePath);
+
+                ShowStats(image, compRle,
+                    encodeT, decodeT,
+                    readT, writeT,
+                    "BMP TO BMP/RLE", args[0],
+                    bmpRlePath);
+
+                Console.WriteLine("\n\n\n");
+
+                #endregion
+
+                #region TiffLzw
+                var tiffLzwPath = "encLzw.tif";
+                Bitmap compTiff;
+
+                encoderParameters = new EncoderParameters
+                {
+                    Param = new[]
+                    {
+                        new EncoderParameter(Encoder.Compression, (long)EncoderValue.CompressionLZW)
+                    }
+                };
+
+                (compTiff, encodeT, decodeT, readT, writeT) = GetCompressedWithStats(
+                    image,
+                    ImageFormat.Tiff,
+                    encoderParameters,
+                    tiffLzwPath);                
+
+                ShowStats(image, compTiff,
+                    encodeT, decodeT,
+                    readT, writeT,
+                    "Bmp to tiff/lzw", args[0],
+                    tiffLzwPath);
+
+                Console.WriteLine("\n\n\n");
+                #endregion
+
+                #region Jpeg  
+                var jpegPath = "jpeg.jpeg";
+                Bitmap compJpeg;
+
+                encoderParameters = new EncoderParameters
+                {
+                    Param = new[]
+                    {
+                        new EncoderParameter(Encoder.Quality, 0L)
+                    }
+                };
+
+                (compJpeg, encodeT, decodeT, readT, writeT) = GetCompressedWithStats(
+                    image,
+                    ImageFormat.Jpeg,
+                    encoderParameters,
+                    jpegPath);
+
+                ShowStats(image, compJpeg,
+                    encodeT, decodeT,
+                    readT, writeT,
+                    "Bmp to jpeg", args[0],
+                    jpegPath);
+
+                Console.WriteLine("\n\n\n");
+
+                #endregion
+
+                Console.WriteLine($"\t\tColor loss stats \n\n".ToUpper());
+
+                BigInteger init, compressRle, compressLzw, compressJpeg;
+
+                init = GetPixelColorSumNew(image);
+
+                compressRle = GetPixelColorSumNew(compRle);
+                Console.WriteLine($"\tBmp/rle color loss : {init - compressRle}\n");
+
+                compressLzw = GetPixelColorSumNew(compTiff);
+                Console.WriteLine($"\tTiff color loss : {init - compressLzw}\n");
+
+                compressJpeg = GetPixelColorSumNew(compJpeg);
+                Console.WriteLine($"\tJpeg color loss : {init - compressJpeg}\n");
+
+                var (initR, initG, initB) = GetPixelRgbSums(image);
+
+                Console.WriteLine($"\n\n\t\tColor loss RGB stats \n\n");
+
+                var (rleR, rleG, rleB) = GetPixelRgbSums(compRle);
+                Console.WriteLine($"\tBmp/rle color loss\t  R:{initR - rleR}; G:{initG-rleG}; B:{initB-rleB}\n");
+
+                var (tiffR, tiffG, tiffB) = GetPixelRgbSums(compTiff);
+                Console.WriteLine($"\tTiff color loss\t  R:{initR - tiffR}; G:{initG-tiffG}; B:{initB - tiffB}\n");
+
+                var (jpegR, jpegG, jpegB) = GetPixelRgbSums(compJpeg);
+                Console.WriteLine($"\tJpeg color loss\t  R:{initR - jpegR}; G:{initG - jpegG}; B:{initB - jpegB}\n");
+
+                GenerateRgbDiffImages(image, compRle, "Rle");
+
+                GenerateRgbDiffImages(image, compTiff, "Tiff");
+
+                GenerateRgbDiffImages(image, compJpeg, "Jpeg");
+            }
+        }
+
+        static void GenerateRgbDiffImages(Bitmap init, Bitmap compressed, string compName)
+        {
+            var rDiff = new Bitmap(init);
+            var gDiff = new Bitmap(init);
+            var bDiff = new Bitmap(init);
+            var rgbDiff = new Bitmap(init);
+
+            for (int i = 0; i < rDiff.Height; i++)
+            {
+                for (int j = 0; j < rDiff.Width; j++)
+                {
+                    var compr = compressed.GetPixel(j, i);
+
+                    var rgb = rDiff.GetPixel(j, i);
+                    rgb = Color.FromArgb(Math.Abs(rgb.R - compr.R), rgb.G, rgb.B);
+                    rDiff.SetPixel(j, i, rgb);
+
+                    rgb = gDiff.GetPixel(j, i);
+                    rgb = Color.FromArgb(rgb.R, Math.Abs(rgb.G - compr.G), rgb.B);
+                    gDiff.SetPixel(j, i, rgb);
+
+                    rgb = bDiff.GetPixel(j, i);
+                    rgb = Color.FromArgb(rgb.R, rgb.G, Math.Abs(rgb.B - compr.B));
+                    bDiff.SetPixel(j, i, rgb);
+
+                    rgb = rgbDiff.GetPixel(j, i);
+                    rgb = Color.FromArgb(Math.Abs(rgb.R - compr.R), Math.Abs(rgb.G - compr.G), Math.Abs(rgb.B - compr.B));
+                    rgbDiff.SetPixel(j, i, rgb);
+                }
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                rDiff.Save(ms, init.RawFormat);
+                rDiff = (Bitmap)Image.FromStream(ms);
+                rDiff.Save($"rgbDiff/rDif{compName}.bmp");
+
+                ms.Flush();
+                ms.Seek(0, SeekOrigin.Begin);
+
+                gDiff.Save(ms, init.RawFormat);
+                gDiff = (Bitmap)Image.FromStream(ms);
+                gDiff.Save($"rgbDiff/gDif{compName}.bmp");
+
+                ms.Flush();
+                ms.Seek(0, SeekOrigin.Begin);
+
+                bDiff.Save(ms, init.RawFormat);
+                bDiff = (Bitmap)Image.FromStream(ms);
+                bDiff.Save($"rgbDiff/bDif{compName}.bmp");
+
+                ms.Flush();
+                ms.Seek(0, SeekOrigin.Begin);
+
+                rgbDiff.Save(ms, init.RawFormat);
+                rgbDiff = (Bitmap)Image.FromStream(ms);
+                rgbDiff.Save($"rgbDiff/rgbDif{compName}.bmp");
+            }
+
+            rDiff.Dispose();
+        } 
+
+        static (BigInteger r, BigInteger g, BigInteger b) GetPixelRgbSums(Bitmap image) =>
+            (GetPixelColorSumNew(image, 2), GetPixelColorSumNew(image, 1), GetPixelColorSumNew(image, 0));        
+        
+        static void ShowStats(Bitmap initial, Bitmap compressed, 
+            double encodeTime, double readTime, 
+            double writeTime, double decodeTime, 
+            string compressionName, string pathInit, 
+            string pathComp)
+        {
+            Console.WriteLine($"\t\t{compressionName}\n");
+
+            long initS, compS;
+
+            using (var fss = File.OpenRead(pathComp))
+            using (var fs = File.OpenRead(pathInit))
+            {
+                initS = fs.Length;
+                compS = fss.Length;
+            }
+
+            Console.WriteLine($"\tSize comparison - ${compressionName}\n");
+            Console.WriteLine($"Initial size: {initS}\t Size after compression {compS}\n");
+            Console.WriteLine($"\tCompression stats(in milliseconds) \n\n EncodeTime : {encodeTime}; Decode time: {decodeTime}; " +
+                $"Read time : {readTime}; Write time : {writeTime}\n");
+        }
+
+        static (Bitmap image, double encodeTimeMs, double decodeTimeMs, double readTimeMs, double writeTimeMs) GetCompressedWithStats(
+            Bitmap image,
+            ImageFormat imageFormat,
+            EncoderParameters encoderParameters,
+            string writePath)
+        {
+            Bitmap compressed;
+            double encodeMs, decodeMs, readMs, writeMs;
+            DateTime start, end;
+
+            //encode
+            (compressed, encodeMs) = Compress(image, imageFormat, encoderParameters);
+            
+            //write
+            start = DateTime.Now;
+            compressed.Save(writePath);
+            end = DateTime.Now;
+            writeMs = (end - start).TotalMilliseconds;
+
+            //read
+            start = DateTime.Now;
+            compressed = (Bitmap)Image.FromFile(writePath);
+            end = DateTime.Now;
+            readMs = (end - start).TotalMilliseconds;
+
+            //decode
+            start = DateTime.Now;
+            compressed.Save($"decoded{imageFormat.ToString()}.bmp", image.RawFormat);
+            end = DateTime.Now;
+            decodeMs = (end - start).TotalMilliseconds;
+
+            return (compressed, encodeMs, decodeMs, readMs, writeMs);
+        }
+
+        static BigInteger GetPixelColorSum(Bitmap image, int? ind = null)
+        {
+            var bitmapData = image.LockBits(
+                new Rectangle(0, 0, image.Width, image.Height), 
+                ImageLockMode.ReadOnly, 
+                image.PixelFormat);
+
+            var byteArr = new byte[bitmapData.Stride * bitmapData.Height];
+
+            Marshal.Copy(bitmapData.Scan0, byteArr, 0, byteArr.Length);
+
+            var sum = new BigInteger();
+
+            if (!ind.HasValue)
+            {
+                for(int i = 0; i < byteArr.Length; 
+                    i += Bitmap.GetPixelFormatSize(image.PixelFormat)/8)
+                {
+                    sum += Color.FromArgb(byteArr[i+2], byteArr[i+1], byteArr[i]).ToArgb();
+                }
+            }
+            else
+            {
+                int indd = ind.Value;
+                for (int i = 0; i < byteArr.Length;
+                    i += Bitmap.GetPixelFormatSize(image.PixelFormat) / 8)
+                {
+                    sum += byteArr[i + indd];
+                }
+            }
+                        
+
+            image.UnlockBits(bitmapData);
+
+            return BigInteger.Abs(sum);
+        }
+
+        static BigInteger GetPixelColorSumNew(Bitmap image, int? ind = null)
+        {
+            var sum = new BigInteger();
+
+
+            if (!ind.HasValue)
+            {
+                Color temp;
+                for (int i = 0; i < image.Height; i++)
+                {
+                    for (int j = 0; j < image.Width; j++)
+                    {
+                        temp = image.GetPixel(j, i);
+
+                        sum += Color.FromArgb(temp.R, temp.G, temp.B).ToArgb();
+                    }
+                }
+            }
+            else
+            {
+                Color temp;
+                for (int i = 0; i < image.Height; i++)
+                {
+                    for (int j = 0; j < image.Width; j++)
+                    {
+                        temp = image.GetPixel(j, i);
+
+                        switch(ind.Value)
+                        {
+                            case 2:
+                                sum += temp.R;
+                                break;
+                            case 1:
+                                sum += temp.G;
+                                break;
+                            case 0:
+                                sum += temp.B;
+                                break;
+                        }
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        static (Bitmap image, double timeMs) Compress(Bitmap image, ImageFormat format, EncoderParameters encoderParameters)
+        {
+            Bitmap compressed;
+            DateTime start, end;
+            double compressTime;
+
+            var imageType = ImageCodecInfo.GetImageEncoders().FirstOrDefault(e => e.FormatID == format.Guid);
+
+            using(var stream = new MemoryStream())            
+            {
+                start = DateTime.Now;
+                image.Save(stream, imageType, encoderParameters);
+                end = DateTime.Now;
+                compressTime = (end - start).TotalMilliseconds;
+                compressed = new Bitmap(Image.FromStream(stream));
+            }
+
+            return (compressed, compressTime);
+        }        
+    }
+}
---
+++ ImageProc/imageproc2/Program.cs
@@ -0,0 +1,450 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace imageproc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var image = (Bitmap)Image.FromFile(args[0]))
+            {
+                var hists = GetColorFrequenciesRgb(image);
+
+                CreateHistBmp(hists[0], "redHist.bmp", Pens.Red);
+
+                CreateHistBmp(hists[1], "greenHist.bmp", Pens.Green);
+
+                CreateHistBmp(hists[2], "blueHist.bmp", Pens.Blue);
+
+                var equalized = Equalize(image);
+
+                hists = GetColorFrequenciesRgb(equalized);
+
+                CreateHistBmp(hists[0], "redHistEqualized.bmp", Pens.Red);
+
+                CreateHistBmp(hists[1], "greenHistEqualized.bmp", Pens.Green);
+
+                CreateHistBmp(hists[2], "blueHistEqualized.bmp", Pens.Blue);
+
+                equalized.Save("Equalized.bmp");
+
+                var robertsImage = Roberts(image);
+                robertsImage.Save("roberts.bmp");
+
+                var sobelImage = Sobel(image);
+                sobelImage.Save("sobel.bmp");
+
+                var previtImage = Previt(image);
+                previtImage.Save("previt.bmp");
+            }
+        }
+        public static Bitmap Roberts(Bitmap original)
+        {
+            Bitmap result = new Bitmap(original.Width, original.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            int width = original.Width;
+            int height = original.Height;
+
+            int[,] GX = new int[,]
+            {
+                { 0, -1 },
+                { 1,  0 }
+            };
+            int[,] GY = new int[,]
+            {
+                { -1, 0 },
+                {  0, 1 }
+            };
+
+            int[,] R = new int[width, height];
+            int[,] G = new int[width, height];
+            int[,] B = new int[width, height];
+            for (int i = 0; i < width; ++i)
+            {
+                for (int j = 0; j < height; ++j)
+                {
+                    R[i, j] = original.GetPixel(i, j).R;
+                    G[i, j] = original.GetPixel(i, j).G;
+                    B[i, j] = original.GetPixel(i, j).B;
+                }
+            }
+
+            int Rx = 0, Ry = 0;
+            int Gx = 0, Gy = 0;
+            int Bx = 0, By = 0;
+            int RChannel, GChannel, BChannel;
+            for (int i = 1; i < original.Width - 1; ++i)
+            {
+                for (int j = 1; j < original.Height - 1; ++j)
+                {
+
+                    Rx = 0;
+                    Ry = 0;
+                    Gx = 0;
+                    Gy = 0;
+                    Bx = 0;
+                    By = 0;
+                    RChannel = 0;
+                    GChannel = 0;
+                    BChannel = 0;
+                    for (int x = -1; x < 1; ++x)
+                    {
+                        for (int y = -1; y < 1; ++y)
+                        {
+                            RChannel = R[i + y, j + x];
+                            Rx += GX[x + 1, y + 1] * RChannel;
+                            Ry += GY[x + 1, y + 1] * RChannel;
+
+                            GChannel = G[i + y, j + x];
+                            Gx += GX[x + 1, y + 1] * GChannel;
+                            Gy += GY[x + 1, y + 1] * GChannel;
+
+                            BChannel = B[i + y, j + x];
+                            Bx += GX[x + 1, y + 1] * BChannel;
+                            By += GY[x + 1, y + 1] * BChannel;
+                        }
+                    }
+                    result.SetPixel(i, j, Color.FromArgb(SatureCast(Rx + Ry), SatureCast(Gx + Gy), SatureCast(Bx + By)));
+                }
+            }
+            return result;
+
+        }
+
+        public static Bitmap Previt(Bitmap original)
+        {
+            Bitmap result = new Bitmap(original.Width, original.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            int width = original.Width;
+            int height = original.Height;
+
+            int[,] GX = new int[,]
+            {
+                { 1, 0, -1 },
+                { 1, 0, -1 },
+                { 1, 0, -1 }
+            };
+            int[,] GY = new int[,]
+            {
+                { -1, -1, -1 },
+                { 0, 0, 0 },
+                { 1, 1, 1 }
+            };
+
+            int[,] R = new int[width, height];
+            int[,] G = new int[width, height];
+            int[,] B = new int[width, height];
+            for (int i = 0; i < width; ++i)
+            {
+                for (int j = 0; j < height; ++j)
+                {
+                    R[i, j] = original.GetPixel(i, j).R;
+                    G[i, j] = original.GetPixel(i, j).G;
+                    B[i, j] = original.GetPixel(i, j).B;
+                }
+            }
+
+            int Rx = 0, Ry = 0;
+            int Gx = 0, Gy = 0;
+            int Bx = 0, By = 0;
+            int RChannel, GChannel, BChannel;
+            for (int i = 1; i < original.Width - 1; ++i)
+            {
+                for (int j = 1; j < original.Height - 1; ++j)
+                {
+
+                    Rx = 0;
+                    Ry = 0;
+                    Gx = 0;
+                    Gy = 0;
+                    Bx = 0;
+                    By = 0;
+                    RChannel = 0;
+                    GChannel = 0;
+                    BChannel = 0;
+                    for (int x = -1; x < 2; ++x)
+                    {
+                        for (int y = -1; y < 2; ++y)
+                        {
+                            RChannel = R[i + y, j + x];
+                            Rx += GX[x + 1, y + 1] * RChannel;
+                            Ry += GY[x + 1, y + 1] * RChannel;
+
+                            GChannel = G[i + y, j + x];
+                            Gx += GX[x + 1, y + 1] * GChannel;
+                            Gy += GY[x + 1, y + 1] * GChannel;
+
+                            BChannel = B[i + y, j + x];
+                            Bx += GX[x + 1, y + 1] * BChannel;
+                            By += GY[x + 1, y + 1] * BChannel;
+                        }
+                    }
+                    result.SetPixel(i, j, Color.FromArgb(SatureCast(Rx + Ry), SatureCast(Gx + Gy), SatureCast(Bx + By)));
+                }
+            }
+            return result;
+        }
+
+        public static Bitmap Sobel(Bitmap original)
+        {
+            Bitmap result = new Bitmap(original.Width, original.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            int width = original.Width;
+            int height = original.Height;
+
+            int[,] GX = new int[,]
+            {
+                { 1, 0, -1 },
+                { 2, 0, -2 },
+                { 1, 0, -1 }
+            };
+            int[,] GY = new int[,]
+            {
+                { -1, -2, -1 },
+                {  0,  0,  0 },
+                {  1,  2,  1 }
+            };
+
+            int[,] R = new int[width, height];
+            int[,] G = new int[width, height];
+            int[,] B = new int[width, height];
+            for (int i = 0; i < width; ++i)
+            {
+                for (int j = 0; j < height; ++j)
+                {
+                    R[i, j] = original.GetPixel(i, j).R;
+                    G[i, j] = original.GetPixel(i, j).G;
+                    B[i, j] = original.GetPixel(i, j).B;
+                }
+            }
+
+            int Rx = 0, Ry = 0;
+            int Gx = 0, Gy = 0;
+            int Bx = 0, By = 0;
+            int RChannel, GChannel, BChannel;
+            for (int i = 1; i < original.Width - 1; ++i)
+            {
+                for (int j = 1; j < original.Height - 1; ++j)
+                {
+
+                    Rx = 0;
+                    Ry = 0;
+                    Gx = 0;
+                    Gy = 0;
+                    Bx = 0;
+                    By = 0;
+                    RChannel = 0;
+                    GChannel = 0;
+                    BChannel = 0;
+                    for (int x = -1; x < 2; ++x)
+                    {
+                        for (int y = -1; y < 2; ++y)
+                        {
+                            RChannel = R[i + y, j + x];
+                            Rx += GX[x + 1, y + 1] * RChannel;
+                            Ry += GY[x + 1, y + 1] * RChannel;
+
+                            GChannel = G[i + y, j + x];
+                            Gx += GX[x + 1, y + 1] * GChannel;
+                            Gy += GY[x + 1, y + 1] * GChannel;
+
+                            BChannel = B[i + y, j + x];
+                            Bx += GX[x + 1, y + 1] * BChannel;
+                            By += GY[x + 1, y + 1] * BChannel;
+                        }
+                    }
+                    result.SetPixel(i, j, Color.FromArgb(SatureCast(Rx + Ry), SatureCast(Gx + Gy), SatureCast(Bx + By)));
+                }
+            }
+            return result;
+        }
+
+        static int SatureCast(double number)
+        {
+            if (number < 0)
+            {
+                return 0;
+            }
+            if (number > 255)
+            {
+                return 255;
+            }
+            return (int)number;
+        }
+
+        static Bitmap Equalize(Bitmap image)
+        {            
+            var height = image.Height;
+            var width = image.Width;
+
+            var outputImage = new Bitmap(width, height, image.PixelFormat);
+
+            var hists = GetColorFrequenciesRgb(image);
+
+            var cdfR = Cdf(hists[0]);
+            var cdfG = Cdf(hists[1]);
+            var cdfB = Cdf(hists[2]);
+
+            var cdfRMin = cdfR.Min(e => e.Value);
+            var cdfGMin = cdfG.Min(e => e.Value);
+            var cdfBMin = cdfB.Min(e => e.Value);
+
+            var bitmapData = image.LockBits(
+                new Rectangle(0, 0, image.Width, image.Height), 
+                ImageLockMode.ReadOnly, 
+                PixelFormat.Format24bppRgb);
+
+            var bytes = new byte[bitmapData.Stride * bitmapData.Height];
+
+            Marshal.Copy(bitmapData.Scan0, bytes, 0, bytes.Length);
+
+            image.UnlockBits(bitmapData);
+
+            var offset = bitmapData.Stride - bitmapData.Width * 3;
+
+            var index = 0;
+
+            for (int i = 0; i < image.Height; i++)
+            {
+                for (int j = 0; j < image.Width; j++, index += 3)
+                {
+                    bytes[index + 2] = H(bytes[index + 2], cdfR, cdfRMin, width, height);
+                    bytes[index + 1] = H(bytes[index + 1], cdfG, cdfGMin, width, height);
+                    bytes[index] = H(bytes[index], cdfB, cdfBMin, width, height);
+                }
+
+                index += offset;
+            }
+
+            bitmapData = outputImage.LockBits(
+                new Rectangle(0, 0, outputImage.Width, outputImage.Height),
+                ImageLockMode.WriteOnly,
+                image.PixelFormat);
+
+            Marshal.Copy(bytes, 0, bitmapData.Scan0, bytes.Length);
+
+            outputImage.UnlockBits(bitmapData);
+
+            return outputImage;
+        }
+
+        static byte H(int v, Dictionary<int, int> cdf, int cdfMin, int w, int h)
+        {
+            var range = 256;
+
+            var hh = ((cdf[v] - cdfMin) / (float)(w * h - cdfMin)) * (range - 1);
+
+            return (byte)Math.Floor(hh);
+        }
+
+        static Dictionary<int, int> Cdf(Dictionary<int, int> colorFreq)
+        {
+            var dict = new Dictionary<int, int>();
+
+            var newDictFreq = new Dictionary<int, int>();
+
+            foreach (var kvPair in colorFreq.Where(e => e.Value != 0))
+            {
+                newDictFreq.Add(kvPair.Key, kvPair.Value);
+            }
+
+            var firstElem = newDictFreq.ElementAt(0);
+
+            dict.Add(firstElem.Key, firstElem.Value);
+
+            for (int i = 1; i < newDictFreq.Keys.Count; i++)
+            {
+                var currEl = newDictFreq.ElementAt(i);
+
+                dict.Add(currEl.Key, currEl.Value + dict.ElementAt(i - 1).Value);
+            }
+
+            return dict;
+        }
+
+        static void CreateHistBmp(Dictionary<int, int> histData, string filePath, Pen pen)
+        {
+            var max = histData.Max(e => e.Value);
+
+            var sum = histData.Sum(e => e.Value);
+
+            var width = 256;
+
+            var height = 256;
+
+            var bmp = new Bitmap(width, height);
+
+            using(Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+                foreach (var key in histData.Keys)
+                {
+                    float y2 = height - (histData[key] / (float)max) * (height - 5);
+                           
+                    g.DrawLine(pen, key, height, key, y2);
+                }
+            }
+
+            using(MemoryStream ms = new MemoryStream())
+            {
+                bmp.Save(ms, ImageFormat.Bmp);
+
+                bmp = new Bitmap(Image.FromStream(ms));
+            }
+
+            bmp.Save(filePath);
+
+            bmp.Dispose();
+        }
+
+        static List<Dictionary<int, int>> GetColorFrequenciesRgb(Bitmap image)
+        {
+            var listDictRgb = new List<Dictionary<int, int>>
+            {
+                new Dictionary<int, int>(),
+                new Dictionary<int, int>(),
+                new Dictionary<int, int>()
+            };
+
+            listDictRgb.ForEach(l =>
+            {
+                for (int i = 0; i < 256; i++)
+                {
+                    l.Add(i, 0);
+                }
+            });
+
+            var bitmapData = image.LockBits(
+                new Rectangle(0, 0, image.Width, image.Height), 
+                ImageLockMode.ReadOnly, 
+                PixelFormat.Format24bppRgb);
+
+            var bytes = new byte[bitmapData.Stride * bitmapData.Height];
+
+            Marshal.Copy(bitmapData.Scan0, bytes, 0, bytes.Length);
+
+            var offset = bitmapData.Stride - bitmapData.Width * 3;
+
+            image.UnlockBits(bitmapData);
+
+            var index = 0;
+
+            for (int i = 0; i < image.Height; i++)
+            {
+                for (int j = 0; j < image.Width; j++, index += 3)
+                {
+                    listDictRgb[0][bytes[index + 2]] += 1;
+                    listDictRgb[1][bytes[index + 1]] += 1;
+                    listDictRgb[2][bytes[index]] += 1;
+                }
+
+                index += offset;
+            }
+
+            return listDictRgb;
+        }
+    }
+}
